7b4511c83d08179e5c4e73e65571d4295555496a
jonathan <jonathan@jonathan-ThinkCentre-M72e>
2017-01-25 16:40:39 -0500

    certificado ingresos

---
+++ src/ArdidBundle/Controller/Consulta/certificadoIngresosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ArdidBundle\Controller\Consulta;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\EntityRepository;
+use ArdidBundle\Entity\Empleado;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class certificadoIngresosController extends Controller {
+
+    /**
+     * @Route("/consulta/certificadoIngresos", name="certificado_ingresos")
+     */
+    public function laboralAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $arUsuario = $this->getUser();
+        $arEmpleado = new \ArdidBundle\Entity\Empleado();
+        $arEmpleado = $em->getRepository('ArdidBundle:Empleado')->findOneBy(array('identificacionNumero' => $arUsuario->getUsername()));
+        $arContratos = $em->getRepository('ArdidBundle:contrato')->findBy(array('codigoEmpleadoFk' => $arEmpleado->getCodigoEmpleadoPk()));
+        $form = $this->createFormBuilder()
+                ->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {            
+            if($request->request->get('opImprimir')) {
+                $codigoContrato = $request->request->get('opImprimir');
+                $objFormatoPago = new \ArdidBundle\Formato\FormatoCertificadoIngresos();
+                $objFormatoPago->Generar($em, $codigoContrato);               
+            }
+        }
+        return $this->render('ArdidBundle:Consulta:certificadoIngresos.html.twig', array(
+                    'arContratos' => $arContratos,
+                    'form'=> $form->createView()
+        ));
+    }
+}
